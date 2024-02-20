19e479b2e1df4b0993497bad56ddc953909a43fb
ZahirBCH <zahir.bachnou@protonmail.com>
2024-02-20 15:18:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,26 +9,26 @@ resource "google_container_cluster" "primary" {
   name     = "my-lab-cluster"
   location = "europe-west9"
 
-  # Définir les caractéristiques du cluster
-  node_pool {
-    name       = "default-pool"
-    machine_type = "e2-small"
-    disk_size_gb = 10
-    disk_type = "pd-standard"
-    initial_node_count = 1
-    node_count = 3
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
 
-    # Configuration des spécificités du pool de nœuds
-    node_config {
-      preemptible  = false
-      oauth_scopes = [
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring",
-      ]
-    }
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-
-  # Définir la version de Kubernetes
-  min_master_version = "1.21"
-  master_version     = "1.21"
 }
