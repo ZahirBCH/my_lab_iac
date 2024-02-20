8375d5126398c8fbb97efe08c7f61d4400d20f16
ZahirBCH <zahir.bachnou@protonmail.com>
2024-02-20 12:25:35 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  credentials = file("GOOGLE_APPLICATION_CREDENTIALS")
+  project = "my-lab-production"
+  region  = "europe-west9"
+  zone    = "europe-west9-a"
+}
+
+resource "google_container_cluster" "my_lab_cluster" {
+  name     = "my-lab-cluster"
+  location = "europe-west9"
+
+  # Définir les caractéristiques du cluster
+  node_pool {
+    name       = "default-pool"
+    machine_type = "e2-small"
+    disk_size_gb = 10
+    disk_type = "pd-standard"
+    initial_node_count = 1
+    node_count = 3
+
+    # Configuration des spécificités du pool de nœuds
+    node_config {
+      preemptible  = false
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+    }
+  }
+
+  # Définir la version de Kubernetes
+  min_master_version = "1.21"
+  master_version     = "1.21"
+}
