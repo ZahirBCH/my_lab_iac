da25542ff8da33ecaf2c9b11b27a0fe9daef476a
ZahirBCH <zahir.bachnou@protonmail.com>
2024-02-20 15:53:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "my-lab-cluster"
-  location = "us-central1"
+  location = "europe-west9-a"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
